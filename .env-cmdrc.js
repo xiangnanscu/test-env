a2d1dbd1b08c2c56eeaca02cf2f565889bca0167
xiangnan <280145668@qq.com>
2022-11-26 19:58:22 +0800

    message:  2022-11-26_19:58:22

---
+++ .env-cmdrc.js
@@ -0,0 +1,12 @@
+const env = {
+  "base": {
+    foo: "bar"
+  },
+  "development": {
+    "NODE_ENV": "development",
+  },
+  "production": {
+    "NODE_ENV": "production",
+  }
+}
+module.exports = env
